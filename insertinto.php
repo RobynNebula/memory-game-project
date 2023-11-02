74b3ff9c1149fa20969fb43a2a5d6e8d97145217
ilonaroussel <ilonarousselpro@gmail.com>
2023-11-02 11:18:27 +0100

    US3

---
+++ insertinto.php
@@ -0,0 +1,1 @@
+<?php require_once "utils/database.php"
--- partials/header.php
+++ partials/header.php
@@ -1,11 +1,10 @@
-<?php 
+<?php
 if (!empty($_SESSION['user_id'])) {
     $pdo = connectToDbAndGetPdo();
     $pdoSelectUserConnected = $pdo->prepare('SELECT pseudo FROM users WHERE id = :user_id ');
-    $pdoSelectUserConnected->execute([":user_id" => $_SESSION['user_id']]); 
+    $pdoSelectUserConnected->execute([":user_id" => $_SESSION['user_id']]);
     $pseudoUserConnected = $pdoSelectUserConnected->fetch();
     $pseudoOfUserConnected = strtoupper($pseudoUserConnected->pseudo);
-
 } else {
     $pseudoOfUserConnected = "MON COMPTE";
 } ?>
@@ -14,16 +13,17 @@ if (!empty($_SESSION['user_id'])) {
     <section>
         <a href="thepowerofmemory.html">The power of memory</a>
         <div class="linkContainer">
-          <div><a href="<?php echo PROJECT_FOLDER ?>accueil.php" <?= ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'accueil.php') ? 'class="active"' : '' ?> > Accueil </a></div>
-          <div><a href="<?php echo PROJECT_FOLDER ?>login.php" <?= ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'login.php') ? 'class="active"' : '' ?> > login </a></div>
-          <div><a href="<?php echo PROJECT_FOLDER ?>games/memory/game.php" <?= ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/game.php') ? 'class="active"' : '' ?> > Jeu </a></div>
-          <div><a href="<?php echo PROJECT_FOLDER ?>games/memory/scores.php" <?= ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/scores.php') ? 'class="active"' : '' ?> > Scores </a></div>
-          <div><a href="<?php echo PROJECT_FOLDER ?>contact.php" <?= ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'contact.php') ? 'class="active"' : '' ?> > Nous contacter </a></div>
-          <div><a href="<?php echo PROJECT_FOLDER ?>MyAccount.php" <?= ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'connexion.php') ? 'class="active"' : '' ?> >
-            <img class="avatar" src= "<?php echo PROJECT_FOLDER ?>assets/image/avatar-homme.png"/>
-            <?php echo $pseudoOfUserConnected ;?>
-          </a></div>
-          
-         </div>
+            <div><a href="<?php echo PROJECT_FOLDER ?>accueil.php" <?= ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'accueil.php') ? 'class="active"' : '' ?>> Accueil </a></div>
+            <div><a href="<?php echo PROJECT_FOLDER ?>login.php" <?= ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'login.php') ? 'class="active"' : '' ?>> login </a></div>
+            <div><a href="<?php echo PROJECT_FOLDER ?>games/memory/game.php" <?= ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/game.php') ? 'class="active"' : '' ?>> Jeu </a></div>
+            <div><a href="<?php echo PROJECT_FOLDER ?>games/memory/scores.php" <?= ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'games/memory/scores.php') ? 'class="active"' : '' ?>> Scores </a></div>
+            <div><a href="<?php echo PROJECT_FOLDER ?>contact.php" <?= ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'contact.php') ? 'class="active"' : '' ?>> Nous contacter </a></div>
+            <div><a href="<?php echo PROJECT_FOLDER ?>MyAccount.php" <?= ($_SERVER['PHP_SELF'] == PROJECT_FOLDER . 'connexion.php') ? 'class="active"' : '' ?>>
+                    <img class="avatar" src="<?php echo PROJECT_FOLDER ?>assets/image/avatar-homme.png" />
+                    <?php echo $pseudoOfUserConnected; ?>
+                </a></div>
+
+        </div>
     </section>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 </header>
